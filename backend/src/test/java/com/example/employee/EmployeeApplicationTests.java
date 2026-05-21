1f1631aee8215aa6342d7297d5099cd263f6c8a7
Mmpstcc <163850682+Mmpstcc@users.noreply.github.com>
2026-05-21 10:40:35 +0530

    Update EmployeeApplicationTests.java

--- backend/src/test/java/com/example/employee/EmployeeApplicationTests.java
+++ backend/src/test/java/com/example/employee/EmployeeApplicationTests.java
@@ -1,3 +1,4 @@
+package com.example.employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
