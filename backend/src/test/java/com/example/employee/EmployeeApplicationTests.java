cb2e018c98c0e18e474f3914de71f888aead3c81
Mmpstcc <163850682+Mmpstcc@users.noreply.github.com>
2026-05-21 10:39:05 +0530

    Update EmployeeApplicationTests.java

--- backend/src/test/java/com/example/employee/EmployeeApplicationTests.java
+++ backend/src/test/java/com/example/employee/EmployeeApplicationTests.java
@@ -1,15 +1,18 @@
-package com.example.employee;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class EmployeeApplicationTests {
 
+    @MockBean
+    private JavaMailSender mailSender;
+
     @Test
     void contextLoads() {
-        // Verifies the Spring context loads successfully
     }
 }
+
